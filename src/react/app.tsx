0616549853dbb7e457e3fdcf1a4d36f5354a9e16
robinwalterfit <ask@robinwalterfit.com>
2017-12-17 22:15:45 +0100

    Initial Commit

---
+++ src/react/app.tsx
@@ -0,0 +1,27 @@
+
+/**
+ *
+ */
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+class App extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+
+                <h2>Welcome to React with Typescript!</h2>
+
+            </div>
+
+        )
+
+    }
+
+}
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) )
